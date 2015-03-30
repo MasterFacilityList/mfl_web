77352f401fa3d6a480dc06fa70778be064263279
sombo09 <somboriot.kipchilat@savannahinformatics.com>
2015-03-30 15:53:23 +0300

    setting up layout

--- src/assets/template/js/custom.js
+++ src/assets/template/js/custom.js
@@ -7,3 +7,20 @@ $(document).ready(function(e){
 		$('.input-group #search_param').val(param);
 	});
 });
+
+$(document).ready(function(){
+$("#mytable #checkall").click(function () {
+        if ($("#mytable #checkall").is(':checked')) {
+            $("#mytable input[type=checkbox]").each(function () {
+                $(this).prop("checked", true);
+            });
+
+        } else {
+            $("#mytable input[type=checkbox]").each(function () {
+                $(this).prop("checked", false);
+            });
+        }
+    });
+    
+    $("[data-toggle=tooltip]").tooltip();
+});
