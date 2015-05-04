961893117090541fac702ba449b34e2aa1ba73fb
jason <jason.wanjohi@savannahinformatics.com>
2015-05-04 19:31:28 +0300

    Fixed app.spec.js

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -1,7 +1,7 @@
 "use strict";
 
 describe( "Wellness application module test:", function () {
-        beforeEach(module("mflApp"));
+        beforeEach(module("mflwebApp"));
 
         it("should expect the truth to be the truth", inject(function () {
             expect(true).toBeTruthy();
