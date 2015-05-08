dc2f1afd71e0eb00b298298d637615021cbdda5d
jason <jason.wanjohi@savannahinformatics.com>
2015-05-08 20:17:05 +0300

    Commented localStorage code

--- src/app/common/services.js
+++ src/app/common/services.js
@@ -1,19 +1,19 @@
-"use strict";
-
-angular.module("mfl.common.services", [])
-
-    .service("mfl.common.services.localStorage", [function () {
-        this.getItem = function (key) {
-            return JSON.parse(localStorage.getItem(key));
-        };
-        this.setItem = function (key, value) {
-            localStorage.setItem(key, JSON.stringify(value));
-        };
-        this.removeItem = function (key) {
-            localStorage.removeItem(key);
-        };
-        this.clear = function () {
-            localStorage.clear();
-        };
-        return;
-    }]);
+//"use strict";
+//
+//angular.module("mfl.common.services", [])
+//
+//    .service("mfl.common.services.localStorage", [function () {
+//        this.getItem = function (key) {
+//            return JSON.parse(localStorage.getItem(key));
+//        };
+//        this.setItem = function (key, value) {
+//            localStorage.setItem(key, JSON.stringify(value));
+//        };
+//        this.removeItem = function (key) {
+//            localStorage.removeItem(key);
+//        };
+//        this.clear = function () {
+//            localStorage.clear();
+//        };
+//        return;
+//    }]);
