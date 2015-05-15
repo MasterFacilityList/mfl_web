eb82c89a844b749c368e7c14b023ab2c11480df6
Chomba Ng'ang'a <choeringa@gmail.com>
2015-05-15 12:08:13 +0300

    Remove mfl.settings to have a single point of truth for configuration

--- src/app/config.js
+++ src/app/config.js
@@ -3,8 +3,16 @@
     angular.module("mflAppConfig", [
         "ngCookies",
         "sil.grid",
-        "mfl.settings"
+        "sil.api.wrapper"
     ])
+
+    .constant("SERVER_URL", "http://localhost/")
+
+    .config(["SERVER_URL", "apiConfigProvider",
+        function(SERVER_URL, apiConfig){
+            apiConfig.SERVER_URL = SERVER_URL;
+        }
+    ]);
 
     .config(["$httpProvider",function ($httpProvider) {
         $httpProvider.defaults.withCredentials = true;
