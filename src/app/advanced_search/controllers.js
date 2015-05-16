26b8214a64340169782e63e909a8496de37c3f10
ANTONY <antony.owaga@savannahinformatics.com>
2015-05-16 21:55:59 +0300

    Implemented pagination on search results page

--- src/app/advanced_search/controllers.js
+++ src/app/advanced_search/controllers.js
@@ -2,18 +2,28 @@
 (function(angular){
     angular.module("mfl.filtering.controllers", [
         "mfl.filtering.services",
-        "mfl.search.utils"
+        "mfl.search.utils",
+        "mfl.settings"
     ])
     .controller("mfl.filtering.controller", ["$scope","$rootScope",
         "$stateParams", "filteringApi", "facilitiesApi", "downloadApi",
-        "mfl.search.filters.changes","filteringData",
+        "mfl.search.filters.changes","filteringData", "SERVER_URL",
+        "$window",
         function($scope, $rootScope, $stateParams,filterApi,
-            facilitiesApi, downloadApi, changedFilters, filteringData){
+            facilitiesApi, downloadApi, changedFilters, filteringData,
+            SERVER_URL, $window){
         $scope.filter_data = {};
         var initFilterModel = function(){
             $scope.filter = $stateParams;
         };
         initFilterModel();
+        //initializing the counter
+        $scope.counter = {
+            hits : "",
+            prev : "",
+            next : ""
+        };
+        //end of initializing the counter
         $scope.filter = {
             county: [],
             constituency: [],
@@ -124,6 +134,104 @@
         $scope.search_results = true;
         $scope.no_result = false;
         $scope.err = "";
+        //declaring items per page used for pagination
+        $scope.itemsPerPage = 50;
+        $scope.pagination = {
+            active : "",
+            page_count: "",
+            next: "",
+            prev: "",
+            current_page: ""
+        };
+        //creating pagination url_params
+        $scope.makeUrl = function (url, next) {
+            /*var params = url.substring(
+                url.indexOf("?")+1, url.length).split("&");
+            console.log(params, next);*/
+            var name = url.substring(url.indexOf("?")+1, url.indexOf("="));
+            var value = url.substring(url.indexOf("=")+1, url.length);
+            console.log(name, next);
+            return value;
+        };
+        //end of making url
+        //adding pagination function
+        $scope.addPagination = function (page_count, url_nxt, url_prev) {
+            $scope.pagination.active = true;
+            $scope.pagination.page_count = Math.ceil(
+                page_count/$scope.itemsPerPage);
+            //making url for pagination
+            console.log(url_nxt, url_prev);
+            if(!_.isNull(url_nxt)) {
+                $scope.pagination.next = true;
+                $scope.nxt_page = $scope.makeUrl(url_nxt, true);
+            }
+            else{
+                $scope.pagination.next = false;
+                $scope.pagination.current_page = $scope.pagination.page_count;
+            }
+            if(!_.isNull(url_prev)){
+                $scope.pagination.prev = true;
+                $scope.prev_page = $scope.makeUrl(url_prev, false);
+                var default_url = SERVER_URL + "api/facilities/facilities/";
+                if($scope.prev_page === default_url) {
+                    $scope.prev_page = 1;
+                }
+                console.log($scope.prev_page, default_url);
+            }else{
+                $scope.pagination.prev = false;
+                $scope.pagination.current_page = 1;
+                if(!_.isNaN($scope.pagination.current_page)) {
+                    $scope.pagination.current_page =
+                    parseInt($scope.pagination.current_page, 10);
+                }
+            }
+            if($scope.pagination.next){
+                $scope.pagination.current_page = $scope.pagination.next_page-1;
+            }
+        };
+        //end of adding addPagination
+        //setting pagination data
+        $scope.setData = function (head) {
+            if(_.has(head, "results")) {
+                $scope.query_results = head.results;
+                $scope.addPagination(head.count, head.next, head.previous);
+            }
+        };
+        //end of setting pagination data
+
+        //seting function to be called each time in order to set paginated
+        $scope.getData = function (arg) {
+            if(!_.isNaN(arg)) {
+                $scope.paging = {
+                    page : arg
+                };
+                facilitiesApi.facilities.filter($scope.paging)
+                    .success(function (data) {
+                        $scope.query_results = data.results;
+                        $scope.setData(data);
+                    })
+                    .error(function (e) {
+                        console.log(e.error);
+                    });
+            }
+            else {
+                facilitiesApi.facilities.list()
+                    .success(function (data) {
+                        $scope.query_results = data.results;
+                        $scope.setData(data);
+                    })
+                    .error(function (e) {
+                        console.log(e.error);
+                    });
+            }
+        };
+        //end of getting paginated data
+
+        //begining of pagination function
+        $scope.paginate = function (page) {
+            $scope.getData(page);
+        };
+        //end of pagination function
         _.each(["county", "operation_status", "constituency", "facility_type"], function(key){
             $scope.filter_data[key] = filteringData[key].data.results;
             $scope.filter_data.ward = [];
@@ -137,10 +245,12 @@
             });
             return filters;
         };
-        if(_.isEmpty($stateParams)){
+        //results of search archived here
+        if(_.isEmpty($stateParams) || _.isUndefined($stateParams.search)){
             filterApi.facilities.list().success(function(res){
                 $scope.search_results = false;
                 $scope.query_results = res.results;
+                $scope.setData(res);
                 if($scope.query_results.length === 0) {
                     $scope.no_result = true;
                 }
@@ -154,7 +264,11 @@
             var filters = removeEmptyFilters($stateParams);
             filterApi.facilities.filter(filters).success(function(res){
                 $scope.search_results = false;
+                //implementing counting logic here
+                $scope.counter.hits = res.count;
+                //end of implementing counting logic here
                 $scope.query_results = res.results;
+                $scope.setData(res);
                 if($scope.query_results.length === 0) {
                     $scope.no_result = true;
                 }
@@ -164,6 +278,7 @@
                 $scope.alert = err.error;
             });
         }
+        //end of search results listung
         var getChildren = function(api, key, filter){
             api.filter(
                 _.extend(filter, $scope.default_filter))
@@ -284,6 +399,13 @@
             small : "menu",
             large : "displaced"
         };
+        if(_.isUndefined($stateParams.search)) {
+            $scope.no_search_query = false;
+        }
+        else{
+            $scope.no_search_query = true;
+            console.log($scope.no_search_query);
+        }
         //activates off-canvas classes
         $scope.changeClass = function () {
             console.log($scope.class);
@@ -300,20 +422,36 @@
         };
         //exporting to excel functionality
         $scope.excelExport = function () {
-            console.log("Called");
-            $scope.excel_filters = {
-                search : $stateParams.search,
-                format : "excel"
-            };
-            facilitiesApi.facilities.filter($scope.excel_filters)
-                .success(function (data) {
-                    console.log(data, downloadApi);
-                    window.location.href =
-                    "http://localhost:8061/api/common/download/download/xlsx/";
-                })
-                .error(function (e) {
-                    console.log(e.error);
-                });
+            if(!_.isUndefined($stateParams.search)) {
+                $scope.excel_filters = {
+                    search : $stateParams.search,
+                    format : "excel"
+                };
+                facilitiesApi.facilities.filter($scope.excel_filters)
+                    .success(function (data) {
+                        console.log(data, downloadApi);
+                        $window.location.href =
+                        SERVER_URL + "api/common/download/download/xlsx/";
+                    })
+                    .error(function (e) {
+                        console.log(e.error);
+                    });
+            }
+            else {
+                $scope.excel_filters = {
+                    format : "excel"
+                };
+                facilitiesApi.facilities.list($scope.excel_filters)
+                    .success(function (data) {
+                        console.log(data, downloadApi);
+                        $window.location.href =
+                        SERVER_URL + "api/common/download/download/xlsx/";
+                    })
+                    .error(function (e) {
+                        console.log(e.error);
+                    });
+            }
+
         };
     }]);
 })(angular);
