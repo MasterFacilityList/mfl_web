10e4c254fe5f33ec53041f15211ddc4837f3d8a7
ANTONY <antony.owaga@savannahinformatics.com>
2015-05-19 12:38:20 +0300

    Cleaning up settings

--- src/settings.js
+++ src/settings.js
@@ -4,13 +4,10 @@
     var setts = {
         "SERVER_URL": "http://localhost:8061/",
         "CREDZ": {
-            "username": "serikalikuu@mfltest.slade360.co.ke",
-            "password": "serikalikuu",
-            "client_id": "89a15KxjyUktjGWyhCv8j8hHFZM6tWxvBczD9hNa",
-            "client_secret": "GgP1uuFAgPsTu6nMmxNTbiZWynHhRrhrLo535plh" +
-                             "TVWRWtRC51hvujbP9F9HX9I0s3td2JpPPiC3DmkQ" +
-                             "K0bmqjstoUtP71VvGvuKQDHnFLP4apE4mGnrDrXe" +
-                             "BN7tNDwQ"
+            "username": "",
+            "password": "",
+            "client_id": "",
+            "client_secret": ""
         }
     };
 
