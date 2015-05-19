404b7a7ce2473ddd000b259411aa3785ed6cb4e8
jason <jason.wanjohi@savannahinformatics.com>
2015-05-19 20:19:01 +0300

    Added header ui-view to states

--- src/app/gis/routes.js
+++ src/app/gis/routes.js
@@ -7,6 +7,10 @@
             .state("gis", {
                 url: "/gis",
                 views: {
+                    "header" : {
+                        controller: "mfl.home.controllers.header",
+                        templateUrl : "home/tpls/header.tpl.html"
+                    },
                     "main": {
                         controller: "mfl.gis.controllers.gis",
                         templateUrl: "gis/tpls/country-map.tpl.html"
@@ -23,6 +27,10 @@
             .state("gis_county", {
                 url: "/county/:county_id/:const_boundaries",
                 views: {
+                    "header" : {
+                        controller: "mfl.home.controllers.header",
+                        templateUrl : "home/tpls/header.tpl.html"
+                    },
                     "main": {
                         controller: "mfl.gis.controllers.gis_county",
                         templateUrl: "gis/tpls/county-map.tpl.html"
@@ -45,6 +53,10 @@
             .state("gis_const", {
                 url: "/constituency/:const_id/:ward_boundaries",
                 views: {
+                    "header" : {
+                        controller: "mfl.home.controllers.header",
+                        templateUrl : "home/tpls/header.tpl.html"
+                    },
                     "main": {
                         controller: "mfl.gis.controllers.gis_const",
                         templateUrl: "gis/tpls/const-map.tpl.html"
@@ -66,6 +78,10 @@
             }).state("gis_ward", {
                 url: "/ward/:ward_id",
                 views: {
+                    "header" : {
+                        controller: "mfl.home.controllers.header",
+                        templateUrl : "home/tpls/header.tpl.html"
+                    },
                     "main": {
                         controller: "mfl.gis.controllers.gis_ward",
                         templateUrl: "gis/tpls/ward-map.tpl.html"
