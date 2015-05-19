6b380247132d0256646d0848ebb302ad90678838
ANTONY <antony.owaga@savannahinformatics.com>
2015-05-19 20:32:29 +0300

    Resolving backend

--- src/settings.js
+++ src/settings.js
@@ -4,13 +4,16 @@
     var setts = {
         "SERVER_URL": "http://localhost:8061/",
         "CREDZ": {
-            "username": "",
-            "password": "",
-            "client_id": "",
-            "client_secret": ""
+            "username": "serikalikuu@mfltest.slade360.co.ke",
+            "password": "serikalikuu",
+            "client_id": "89a15KxjyUktjGWyhCv8j8hHFZM6tWxvBczD9hNa",
+            "client_secret": "GgP1uuFAgPsTu6nMmxNTbiZWynHhRrhrLo535plh" +
+                             "TVWRWtRC51hvujbP9F9HX9I0s3td2JpPPiC3DmkQ" +
+                             "K0bmqjstoUtP71VvGvuKQDHnFLP4apE4mGnrDrXe" +
+                             "BN7tNDwQ"
         }
     };
-    
+
     setts.CREDZ.token_url = setts.SERVER_URL + "o/token/";
 
     window.MFL_SETTINGS = setts;
