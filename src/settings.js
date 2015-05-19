3ca657ebc9e5d31113c84c588b14843a85b42e82
Liyosi Collins <collinskivale@gmail.com>
2015-05-19 11:41:40 +0300

    removed settings from code

--- src/settings.js
+++ src/settings.js
@@ -4,12 +4,10 @@
     var setts = {
         "SERVER_URL": "http://localhost:8061/",
         "CREDZ": {
-            "username": "serikalikuu@mfltest.slade360.co.ke",
-            "password": "serikalikuu",
-            "client_id": "GIRNlwTVZOxwjPPVT1vWF3cjt7bSIt7MtVFWqqQ3",
-            "client_secret": "U8ZZjjbocuKMOfeY958n3wwe6mJhbOSVJArw0bYblz88H6IBTta"+
-                "GUI0eTevCLwgBJiFMh3PwQYovpywaXNQB1Yo89BMGVy"+
-                "8pMDHAiXkPYrAwecE4a6YnKtA014PI0R1o"
+            "username": "",
+            "password": "",
+            "client_id": "",
+            "client_secret": ""
         }
     };
 
