9f6e2b014bc850510abc9a9dc4c69a3586f7e389
Liyosi Collins <collinskivale@gmail.com>
2015-05-19 16:18:56 +0300

    removed older directive

--- src/app/advanced_search/directives.js
+++
@@ -1,9 +0,0 @@
-(function(angular){
-    angular.module("mfl.filtering.directives", [])
-    .directive("mflPagination", function(){
-        return {
-            restrict: "EA",
-            templateUrl: "advanced_search/tpls/pagination-directive.tpl.html"
-        };
-    });
-})(angular);
