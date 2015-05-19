4c8fa6b9459c52b902c785027ec0f0f1d44fd16f
jason <jason.wanjohi@savannahinformatics.com>
2015-05-19 20:09:53 +0300

    fixed heatmap issue

--- src/settings.js
+++ src/settings.js
@@ -4,10 +4,11 @@
     var setts = {
         "SERVER_URL": "http://localhost:8061/",
         "CREDZ": {
-            "username": "",
-            "password": "",
-            "client_id": "",
-            "client_secret": ""
+            "username": "serikalikuu@mfltest.slade360.co.ke",
+            "password": "serikalikuu",
+            "client_id": "LzstVEhMcbDpiMovhRb1TjWCMMBDyeiKoqNlqHcO",
+            "client_secret": "0ET4Bj1VMjNcYxgrpZIvIaLOmNsF7C7B0j1SOZVhbu9Qnru0kbZob1y3zOAKiFxw2LX"+
+                            "4PeVvz38w2wRdMw5FKA0DyUJWC4SYADZMTYmytCECbup5wYIdZbwHm332WeYT"
         }
     };
     
