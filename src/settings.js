1d6a87acb0b003c0ea59d2a6bf0733b47c4c81bd
ANTONY <antony.owaga@savannahinformatics.com>
2015-05-20 12:12:02 +0300

    Removed client OAuth2 stuff from settings

--- src/settings.js
+++ src/settings.js
@@ -4,12 +4,9 @@
     var setts = {
         "SERVER_URL": "http://localhost:8061/",
         "CREDZ": {
-            "username": "serikalikuu@mfltest.slade360.co.ke",
-            "password": "serikalikuu",
-            "client_id": "aQtG5rUJKF9GxWQWCuoZc6m5i581i5pCxOEIN7L4",
-            "client_secret": "DZ4fCreUDhij74WdEV6RZ9qOi9O6UzWqYSB8TrmKixo" +
-            "d3WWHMJ6EeVY1DktZVgnAon5Czc2h6Wg8kjce3DiegP3xTfdKh67t2jHHhPyV"+
-            "815K8QjEm12R7xKEIaUcR7dB"
+            "username": "",
+            "password": "",
+            "client_id": ""
         }
     };
 
