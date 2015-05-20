89a02536187d8892c10e979f750fe944510f2aee
ANTONY <antony.owaga@savannahinformatics.com>
2015-05-20 09:16:52 +0300

    Editing settings

--- src/settings.js
+++ src/settings.js
@@ -6,11 +6,15 @@
         "CREDZ": {
             "username": "serikalikuu@mfltest.slade360.co.ke",
             "password": "serikalikuu",
-            "client_id": "89a15KxjyUktjGWyhCv8j8hHFZM6tWxvBczD9hNa",
+            /*"client_id": "89a15KxjyUktjGWyhCv8j8hHFZM6tWxvBczD9hNa",
             "client_secret": "GgP1uuFAgPsTu6nMmxNTbiZWynHhRrhrLo535plh" +
                              "TVWRWtRC51hvujbP9F9HX9I0s3td2JpPPiC3DmkQ" +
                              "K0bmqjstoUtP71VvGvuKQDHnFLP4apE4mGnrDrXe" +
-                             "BN7tNDwQ"
+                             "BN7tNDwQ"*/
+            "client_id": "w3xpORZhHG4RdjTXdZWOi2Zg3LBxiZfGMpaJEkPF",
+            "client_secret": "VEDhTr6CFEPOajWLxRYQBp8oK0c6tBpOu2Jm8Wp79e" +
+            "NHTFu5R0l5NBNnBuxgMuVf5TTY3EEPgAiXpITkqjW3NfF7ZkA7zSl97vSe4dn" +
+            "A9m5e27mqoaVNKod8vjHT1rwk"
         }
     };
 
