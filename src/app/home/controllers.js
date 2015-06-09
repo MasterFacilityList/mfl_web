759e4d10d6a2d7f97650f9b7c708d46fb7825104
ANTONY <antony.owaga@savannahinformatics.com>
2015-06-09 12:50:06 +0300

    Removing code for global spinner

--- src/app/home/controllers.js
+++ src/app/home/controllers.js
@@ -21,23 +21,12 @@
         };
     }])
 
-    .controller("mfl.home.controllers.header", ["$rootScope", "$scope", "$http",
-        function ($rootScope, $scope, $http) {
+    .controller("mfl.home.controllers.header", ["$scope",
+        function ($scope) {
             $scope.tooltip = {
                 "title": "",
                 "checked": false
             };
-            $scope.stateTransition = true;
-
-            $scope.hasPendingRequests = function () {
-                return $http.pendingRequests.length > 0;
-            };
-            $rootScope.$on("$stateChangeStart", function () {
-                $scope.stateTransition = false;
-            });
-            $rootScope.$on("$stateChangeSuccess", function () {
-                $scope.stateTransition = true;
-            });
         }
     ])
     .controller("mfl.home.controllers.facility_details", ["$scope",
