3cb162992d9ff41d4bfc543a6e9f3769d744dcc1
ANTONY <antony.owaga@savannahinformatics.com>
2015-07-07 11:50:50 +0300

    Styling and putting recommended text and header behavior

--- src/settings.js
+++ src/settings.js
@@ -4,8 +4,8 @@
     var setts = {
         "SERVER_URL": "http://localhost:8061/",
         "CREDZ": {
-            "username": "national@mfltest.slade360.co.ke",
-            "password": "national",
+            "username": "public@mfltest.slade360.co.ke",
+            "password": "public",
             "client_id": "xMddOofHI0jOKboVxdoKAXWKpkEQAP0TuloGpfj5",
             "client_secret": "PHrUzCRFm9558DGa6Fh1hEvSCh3C9Lijfq8s" +
                              "bCMZhZqmANYV5ZP04mUXGJdsrZLXuZG4VCmv" +
