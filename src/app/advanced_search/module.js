2c3e2009a8bf4fa2ba7a06d7878ff60ab1b3b4cd
Chomba Ng'ang'a <choeringa@gmail.com>
2015-07-08 11:57:34 +0300

    remove old facility filter page

--- src/app/advanced_search/module.js
+++
@@ -1,9 +0,0 @@
-(function(angular){
-    "use strict";
-    angular.module("mfl.filtering", [
-        "mfl.filtering.controllers",
-        "mfl.filtering.services",
-        "mfl.filtering.routes",
-        "mfl.filtering.directives"
-    ]);
-})(angular);
