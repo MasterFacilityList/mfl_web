aa00ca8ae35072893bd3d0e8dbaa5701051785d5
jason <jason.wanjohi@savannahinformatics.com>
2015-09-25 19:29:59 +0300

    Added map of facility in chul detail

--- src/app/chul/controllers.js
+++ src/app/chul/controllers.js
@@ -1,4 +1,4 @@
-(function(angular){
+(function(angular,_){
     "use strict";
 
     angular.module("mfl.chul.controllers", ["mfl.chul.services",
@@ -23,16 +23,89 @@
         }
     ])
     .controller("mfl.chul.controllers.view", ["$scope","mfl.chul.services.wrappers","$stateParams",
-        function ($scope,wrappers,$stateParams) {
+        "gisAdminUnitsApi","leafletData",
+        function ($scope,wrappers,$stateParams,gisAdminUnitsApi,leafletData) {
             $scope.tooltip = {
                 "title": "",
                 "checked": false
             };
             $scope.spinner = true;
+            /*Setup for map data*/
+            angular.extend($scope, {
+                defaults: {
+                    scrollWheelZoom: false
+                },
+                layers:{},
+                tiles:{
+                    openstreetmap: {
+                        url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        options: {
+                            opacity: 0.7,
+                            attribution: "&copy; <a href='http://www.openstreetmap.org/"+
+                            "copyright'>OpenStreetMap</a> contributors"
+                        }
+                    }
+                }
+            });
             wrappers.chul.get($stateParams.unit_id)
-            .success(function (data) {
+            .success(function (unit) {
                 $scope.spinner = false;
-                $scope.unit = data;
+                $scope.unit = unit;
+                /*ward coordinates*/
+                gisAdminUnitsApi.wards.get(unit.boundaries.ward_boundary)
+                .success(function(data){
+                    $scope.spinner = false;
+                    $scope.ward_gis = data;
+                    leafletData.getMap("wardmap")
+                        .then(function (map) {
+                            var coords = data.properties.bound.coordinates[0];
+                            var bounds = _.map(coords, function(c) {
+                                return [c[1], c[0]];
+                            });
+                            map.fitBounds(bounds);
+                        });
+                    var gis = data;
+                    angular.extend($scope, {
+                        markers: {
+                            mainMarker: {
+                                layer:"facility",
+                                lat: unit.geo_features.geometry.coordinates[1],
+                                lng: unit.geo_features.geometry.coordinates[0],
+                                message: "Facility location"
+                            }
+                        },
+                        geojson: {
+                            data: gis,
+                            style: {
+                                fillColor: "rgb(255, 135, 32)",
+                                weight: 2,
+                                opacity: 1,
+                                color: "rgba(0, 0, 0, 0.52)",
+                                dashArray: "3",
+                                fillOpacity: 0.8
+                            }
+                        },
+                        layers:{
+                            baselayers:{
+                                ward: {
+                                    name: "Ward",
+                                    url: "/assets/img/transparent.png",
+                                    type:"group"
+                                }
+                            },
+                            overlays:{
+                                facility:{
+                                    name:"Facility Location",
+                                    type:"group",
+                                    visible: true
+                                }
+                            }
+                        }
+                    });
+                })
+                .error(function(error){
+                    console.error(error);
+                });
             })
             .error(function (error) {
                 $scope.spinner = false;
@@ -41,4 +114,4 @@
         }
     ]);
 
-})(window.angular);
+})(window.angular,window._);
--- src/app/chul/tests/controllers.spec.js
+++ src/app/chul/tests/controllers.spec.js
@@ -1,4 +1,4 @@
-(function () {
+(function (angular) {
     "use strict";
     describe("Chul controllers: ", function () {
         var rootScope, ctrl, httpBackend, server_url,
@@ -54,24 +54,85 @@
             httpBackend.verifyNoOutstandingExpectation();
         });
 
-        it("should load view controller and data", function () {
+        it("should load view controller and data",
+        inject(["leafletData",
+           function (leafletData) {
             var data = {
                 "$scope": rootScope.$new(),
                 "$stateParams": {
                     unit_id: 1
                 }
             };
+            var unit = {
+                boundaries:{
+                    ward_boundary: 1
+                },
+                geo_features:{
+                    geometry:{
+                        coordinates:[0,1]
+                    }
+                }
+            };
+            var boundary = {
+                    geometry:{
+                        coordinates: {
+                            coordinates:[
+                                [1,2],[3,4]
+                            ]
+                        }
+                    },
+                    properties: {
+                        coordinates: {
+                            coordinates:[
+                                [1,2],[3,4]
+                            ]
+                        },
+                        bound: {
+                            coordinates:[
+                                [
+                                    [1,2],[3,4]
+                                ]
+                            ]
+                        }
+                    }
+                };
+            var obj = {
+                then: angular.noop
+            };
             data.$scope.unit = {
                 facility: 1
             };
-            ctrl("view", data);
             httpBackend
                 .expectGET(server_url+"api/chul/units/1/")
-                .respond(200, {facility:1});
+                .respond(200, unit);
+            httpBackend
+                .expectGET(server_url+"api/gis/ward_boundaries/1/")
+                .respond(200, boundary);
+            ctrl("view", data);
+            spyOn(data.$scope, "$on").andCallThrough();
+            spyOn(leafletData, "getMap").andReturn(obj);
+            spyOn(obj, "then");
+
+            data.$scope.$apply();
+            data.$scope.$digest();
+
             httpBackend.flush();
             httpBackend.verifyNoOutstandingRequest();
             httpBackend.verifyNoOutstandingExpectation();
-        });
+
+            expect(leafletData.getMap).toHaveBeenCalled();
+            expect(obj.then).toHaveBeenCalled();
+
+            var then_fxn = obj.then.calls[0].args[0];
+            expect(angular.isFunction(then_fxn)).toBe(true);
+            var map = {
+                fitBounds: angular.noop
+            };
+            spyOn(map, "fitBounds");
+            then_fxn(map);
+            expect(map.fitBounds).toHaveBeenCalledWith([[2,1],
+                                                        [4,3]]);
+        }]));
 
         it("should test failing to view one chul", function () {
             var data = {
@@ -89,5 +150,34 @@
             httpBackend.verifyNoOutstandingExpectation();
         });
 
+        it("should test fail to load ward boundary details", function () {
+            var data = {
+                "$scope": rootScope.$new(),
+                "$stateParams": {
+                    unit_id: 1
+                }
+            };
+            var unit = {
+                boundaries:{
+                    ward_boundary: 1
+                },
+                geo_features:{
+                    geometry:{
+                        coordinates:[0,1]
+                    }
+                }
+            };
+            ctrl("view", data);
+            httpBackend
+                .expectGET(server_url+"api/chul/units/1/")
+                .respond(200, unit);
+            httpBackend
+                .expectGET(server_url+"api/gis/ward_boundaries/1/")
+                .respond(500,{});
+            httpBackend.flush();
+            httpBackend.verifyNoOutstandingRequest();
+            httpBackend.verifyNoOutstandingExpectation();
+        });
+
     });
-})();
+})(window.angular);
